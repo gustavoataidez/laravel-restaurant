fd7fafff1aa56b4dc84dcda668233572157d14f7
gustavoataidez <luisgustavoataidee@gmail.com>
2023-12-07 18:00:49 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizza', function () {
+    //return view('pizza');
+    $type = ['name' => 'hawaii','flavor' => 'saldy'];
+    return view('pizza', $type);
+});
